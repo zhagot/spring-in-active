7ac732e672ae21d0f68e94559cfc348111b7f9ce
Denis Zhagot <denis.zhagot@leadex.systems.ru>
2023-03-23 00:34:15 +0300

    Chapter 2

---
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest // Тест Spring Boot
+class TacoCloudApplicationTests {
+
+	@Test // Тестовый метод
+	void contextLoads() {
+	}
+
+}
